fadc210916946ff51b82d00bc1a0e3ddd722a72d
ts5516 <ts5516@naver.com>
2021-05-24 14:32:45 +0900

    search modify

---
+++ app/src/main/java/com/onex/onexproject/Frag_Search/searchExhibitionFragment.java
@@ -0,0 +1,26 @@
+package com.onex.onexproject.Frag_Search;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.onex.onexproject.R;
+
+
+public class searchExhibitionFragment extends Fragment {
+
+
+    public searchExhibitionFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_user, container, false);
+    }
+}
---
+++ app/src/main/java/com/onex/onexproject/Frag_Search/searchUserFragment.java
@@ -0,0 +1,27 @@
+package com.onex.onexproject.Frag_Search;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.onex.onexproject.R;
+
+
+public class searchUserFragment extends Fragment {
+
+
+    public searchUserFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_user, container, false);
+    }
+}
