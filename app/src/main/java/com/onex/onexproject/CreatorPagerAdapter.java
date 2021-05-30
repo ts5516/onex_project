8f0c7163504e04fa51eecc47b66e58f818e1c2a4
winter_beom <arhentina98@naver.com>
2021-05-30 17:47:27 +0900

    design changed

---
+++ app/src/main/java/com/onex/onexproject/CreatorPagerAdapter.java
@@ -0,0 +1,52 @@
+package com.onex.onexproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import org.jetbrains.annotations.NotNull;
+
+public class CreatorPagerAdapter extends PagerAdapter {
+    private LayoutInflater inflater;
+    private final int[] images = {
+            R.drawable.creator,
+            R.drawable.creator3,
+            R.drawable.creator4,
+            R.drawable.creator5,
+            R.drawable.creator6};
+    private Context mContext;
+
+    public CreatorPagerAdapter(Context context) {
+        mContext = context;
+    }
+
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull @NotNull View view, @NonNull @NotNull Object object) {
+        return view ==((View)object);
+    }
+    public Object instantiateItem(ViewGroup container, int position)
+    {
+        inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view =inflater.inflate(R.layout.ex1,container,false);
+        ImageView imageView = view.findViewById(R.id.ex1);
+        imageView.setImageResource(images[position]);
+        container.addView(view);
+        return view;
+    }
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object)
+    {
+        container.invalidate();
+    }
+}
--- app/src/main/java/com/onex/onexproject/Frag_Menu/Frag3_Creator.java
+++ app/src/main/java/com/onex/onexproject/Frag_Menu/Frag3_Creator.java
@@ -1,6 +1,45 @@
 package com.onex.onexproject.Frag_Menu;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.onex.onexproject.CreatorPagerAdapter;
+import com.onex.onexproject.Exhibition_hacking;
+import com.onex.onexproject.R;
+import com.onex.onexproject.SearchActivity;
+import com.onex.onexproject.ViewPagerAdapter;
+import com.pm10.library.LineIndicator;
 
 public class Frag3_Creator extends Fragment {
+    ImageView button;
+    Activity activity;
+    Context context;
+    CreatorPagerAdapter adapter;
+    ViewPager viewPager;
+    ImageView btn_lemon;
+    private ViewGroup rootView;
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = (ViewGroup) inflater.inflate(R.layout.menu_frag_creator, container, false);
+        activity =getActivity();
+        context =getContext();
+        viewPager = rootView.findViewById(R.id.viewPager3);
+        adapter = new CreatorPagerAdapter(context);
+        viewPager.setAdapter(adapter);
+        LineIndicator lineIndicator = rootView.findViewById(R.id.line_indicator);
+        lineIndicator.setupWithViewPager(viewPager);
+        return rootView;
+
+    }
 }
